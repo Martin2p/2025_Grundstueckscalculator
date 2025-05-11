d1af78b756ced74ec87d6b9a32dfa43416505bc6
Martin <123328309+Martin2p@users.noreply.github.com>
2025-05-11 10:44:11 +0200

    Create JavaFX_GrundstueckGUI.java

---
+++ JavaFX_GrundstueckGUI.java
@@ -0,0 +1,42 @@
+package src;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class JavaFX_GrundstueckGUI extends Application {
+
+	@Override
+	public void start(Stage meineBuehne) throws Exception {
+		
+		//eine Instanz von FXMLLoader erzeugen
+		FXMLLoader grundstueckrechner = new FXMLLoader(getClass().getResource("fxml_Grundstueckspreise.fxml"));
+		
+		//die Datei laden
+		Parent root = grundstueckrechner.load();
+		
+		//Kontroller beschaffen
+		FXMLController trController = grundstueckrechner.getController();
+		
+		//und die Bühne übergeben
+		trController.setMeineStage(meineBuehne);
+		
+		//Scene erstellen
+		Scene meineScene = new Scene(root, 600, 400);
+		
+		//den Titel über stage setzen
+		meineBuehne.setTitle("JavaFX Grundstückscalculator");
+		//die Szene setzen
+		meineBuehne.setScene(meineScene);
+		//Fenstergröße fix setzen
+		meineBuehne.setResizable(false);
+		//anzeigen
+		meineBuehne.show();
+	}
+	public static void main(String[] args) {
+		//der Start
+		launch(args);
+	}
+}
