87ffc1be60a23ff3a5323a8dad88c689bf46f3ac
Martin <123328309+Martin2p@users.noreply.github.com>
2025-05-11 15:20:33 +0200

    Update FXMLController.java

--- FXMLController.java
+++ FXMLController.java
@@ -6,31 +6,39 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class FXMLController {
 	//Deklarationen
-	@FXML private Stage meineStage;
-	@FXML private TextField lengthIn;
-	@FXML private TextField widthIn;
-	@FXML private TextField qmPriceIn;
-	@FXML private Label ausgabe;
 	
+		@FXML private Stage meineStage;
+		@FXML private TextField lengthIn;
+		@FXML private TextField widthIn;
+		@FXML private TextField qmPriceIn;
+		@FXML private Label ausgabe;
+	
+		
 	//die Methode setzt die Bühne auf den übergebenen Wert
 	public void setMeineStage(Stage meineStage) {
 		this.meineStage = meineStage;
 	}
 	
+	
 	//die allgemeine Berechnen-Methode
 	@FXML protected void berechnen(ActionEvent event) {
 		
+		//Ausgabe der Berechnungen unter Verwendung der Methoden
 		String flaeche = ("Grundstücksfläche: " + flaecheBerechnen() + " m²");
-		String preis = ("Der finale Grundstückspreis beträgt: \n" + preisBerechnen() + " €");
+		String netto = ("Nettopreis: " + nettoPreis() + " €");
 		
-		ausgabe.setText(flaeche + "\n" + preis);
+		//Provision und Endpreis werden auf 2 Nachkommastellen gekürzt angezeigt
+		String provision = ("Preis inklusive Provision: \n" + String.format("%.2f",provisionsPreis()) + " €");
+		String endPreis = ("Preis inkl. Provision und Mehrwertsteuer: \n" + String.format("%.2f", bruttoPreis()) + " €");
+	
+		ausgabe.setText(flaeche + "\n" + netto + "\n" + provision + "\n" + endPreis);
 	}
+	
 	
 	//die Methode zur Berechnung der Fläche
 	public double flaecheBerechnen() {
@@ -46,39 +54,43 @@ public class FXMLController {
 		}
 	}
 	
-	//die Methode zur Berechnung des Grundstückspreises
-	private double preisBerechnen() {
+	//die Methode für den Netto-Grundstückspreis
+	private double nettoPreis() {
+		
+		//falls 0 oder ein anderer ungültiger Wert eingeben wird
+		double qmPrice = Double.parseDouble(qmPriceIn.getText());
 		
 		try {
-			double qmeter = flaecheBerechnen();
-			double qmPrice = Double.parseDouble(qmPriceIn.getText());
-			
-			 //Provision 5%
-			final double PROVISION = 1.05;
-			
-			//Mehrwertsteuer 19%
-			final double MWST = 1.19;
-			
-			//Grundstueckspreis berechnen
-			double preisGrundst = qmPrice * qmeter;
-							
-			//Provisionsaufschlag berechnen
-			double aufschlag = preisGrundst * PROVISION;
-						
-			//finaler Grundstueckspreis inkl. Provision und Mehrwertsteuer
-			double finalPreis = aufschlag * MWST;
-			
-			return Math.round(finalPreis);
+			return flaecheBerechnen() * qmPrice;
 		}
 		catch (Exception e) {
 			return 0;
 		}
 	}
 	
+	//die Methode zur Berechnung des Grundstückspreises inkl. Provision
+	private double provisionsPreis() {
+		
+		//Provision 5%
+		final double PROVISION = 1.05;
+			
+		return PROVISION * nettoPreis();
+	}
+	
+	//die Methode
+	private double bruttoPreis() {
+		//Mehrwertsteuer 19%
+		final double MWST = 1.19;
+		
+		return (MWST * provisionsPreis());
+	}
+	
+
 	//die Methode zum Beenden
 	@FXML protected void beendenKlick(ActionEvent event) {
 		Platform.exit();
 	}
+	
 	
 	//die Methode neu -> Clear
 	@FXML protected void reset(ActionEvent event) {
@@ -93,6 +105,7 @@ public class FXMLController {
 		qmPriceIn.setText(null);
 		ausgabe.setText(null);
 	}
+	
 	
 	//Info
 	@FXML protected void infoKlick(ActionEvent event) {
